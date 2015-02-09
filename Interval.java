5dda24770a3bf54e16f8a9ea628a7559b11533df
Francis <francis.beauchampsc@gmail.com>
2015-02-09 14:28:18 -0500

    Adds interval class

---
+++ Interval.java
@@ -0,0 +1,14 @@
+public class Interval {
+	private long start;
+	private long end;	 
+	
+	public Interval() { 
+		start = System.currentTimeMillis(); 
+	}
+
+	public void stop() { 
+		end = System.currentTimeMillis(); 
+	}
+	
+	public long current() { return end - start; }
+}
