c33c8cb47e6328c995c8ad39f66fca9d37c48b03
Francis <francis.beauchampsc@gmail.com>
2015-02-09 14:16:56 -0500

    Small Changes

--- Sudoku.java
+++ Sudoku.java
@@ -77,7 +77,7 @@ public class Sudoku {
 	@Override
 	public String toString() {
 		
-		String sudoku = new String();
+		String sudoku = "";
 		
 		for (int i = 0; i < SIZE; ++i) {
 			for (int j = 0; j < SIZE; ++j)
